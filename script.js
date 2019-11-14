8fe3bdf452facdcb84c488f7f9cbd3fe217d49f5
mrgts <oscarfelipe2506@gmail.com>
2019-11-13 21:18:58 -0500

    href

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-//defined animation timeline options
 var tmax_opts = {
   delay: 0.3,
-  //repeats infinitely
   repeat: -1,
-  //between repeats, wait 0.5s
   repeatDelay: 0.5,
-  //causes animation to alternate forwards and backwards
   yoyo: true
 };
 var tmax_t1 = new TimelineMax(tmax_opts),
@@ -22,5 +18,4 @@ var staggerTo = {
   scale: 1,
   ease: Elastic.easeInOut
 };
-//GSAP animation expression
 tmax_t1.staggerFromTo(shapes, duration, staggerFrom, staggerTo, stagger, 0);
